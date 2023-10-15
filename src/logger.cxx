5b56a9c08eab5a4553ed0e9a072eea7bb1323f5c
Mura <ope_overlord@hotmail.com>
2023-10-15 23:09:55 +0900

    supports unnamed namespace such as {anonymous}::.

--- src/logger.cxx
+++ src/logger.cxx
@@ -43,7 +43,7 @@ namespace log {
 #if ! defined(xxx_no_logging)
 
 namespace {
-std::regex const function_name_re{R"(^.* ((?:`?[A-Za-z_][-A-Za-z_0-9<>']*::)*~?[A-Za-z_][A-Za-z_0-9<>]*) ?\(.*$)"};
+std::regex const function_name_re{R"(^.* ((?:`?[A-Za-z_{][-A-Za-z_0-9<>'}]*::)*~?[A-Za-z_][A-Za-z_0-9<>]*) ?\(.*$)"};
 }
 
 logger_t::logger_t(level_t level, std::filesystem::path const& path, std::string_view const logger, bool console, bool daily) :
