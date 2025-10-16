43b8e07cb23572a9b7adaf17b6e07bfc5ee78571
Mura <mura.op.overlord@gmail.com>
2025-10-17 02:19:07 +0900

    adds copyrights and third party license.

--- xxx/queue.hxx
+++ xxx/queue.hxx
@@ -20,29 +20,25 @@ namespace xxx {
 template<typename T>
 class queue {
 public:
-	///	@brief	Terminated
-	class terminated_error : public std::runtime_error {
-	public:
-		///	@brief	Constructor.
-		terminated_error() :
-			std::runtime_error("terminated") {}
-	};
-
 	/// @brief 	Enqueues an event.
 	/// @param[in]	t	The event to push.
-	void enqueue(T const& t) {
+	/// @return	It returns true if queued; otherwise, it returns false.
+	bool enqueue(T const& t) {
 		std::lock_guard lock{mutex_};
-		if (finished_) throw terminated_error{};
+		if (finished_) return false;
 		queue_.push_back(t);
 		condition_.notify_one();
+		return true;
 	}
 	/// @brief 	Enqueues an event.
 	/// @param[in]	t	The event to push.
-	void enqueue(T&& t) {
+	/// @return	It returns true if queued; otherwise, it returns false.
+	bool enqueue(T&& t) {
 		std::lock_guard lock{mutex_};
-		if (finished_) throw terminated_error{};
+		if (finished_) return false;
 		queue_.emplace_back(std::move(t));
 		condition_.notify_one();
+		return true;
 	}
 	/// @brief 	Dequeues an event.
 	///		If this queue is empty, this method waits a new event.
